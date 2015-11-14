dd2c64013f0cee7e77b0a1815df0c874f25e0ff1
Alexis Pautrot <alexis@pautrot.com>
2015-11-14 20:23:47 +0100

    Dernières modifications pour merge

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -14,58 +14,58 @@ public enum InputActionName
     Right
 }
 
+[System.Serializable]
+public class InputDefinition
+{
+	public InputActionName name;
+	public GameObject buttonPrefab;
+
+	public string joystickButtonName;
+	public string joystickAxixName;
+	public float joystickAxisThreshold;
+
+	public KeyCode keyboardKeyCode;
+
+	internal void DebugInWindow()
+	{
+		bool isActivated = IsActivated;
+		DebugWindow.Log ( "Input", name.ToString (), isActivated );
+	}
+
+	public bool IsActivated
+	{
+		get
+		{
+			if ( joystickButtonName.Length > 0 )
+			{
+				return Input.GetButton ( joystickButtonName );
+			}
+
+			if ( joystickAxixName.Length > 0 )
+			{
+				float value = Input.GetAxis ( joystickAxixName );
+				if ( joystickAxisThreshold < 0 )
+					return value < joystickAxisThreshold;
+				else
+					return value > joystickAxisThreshold;
+			}
+
+			if ( keyboardKeyCode != KeyCode.None )
+			{
+				return Input.GetKey ( keyboardKeyCode );
+			}
+
+			Debug.LogError ( "Invalid input setup found for button " + name );
+			return false;
+		}
+	}
+}
+
 public class InputManager : SceneSingleton<InputManager>
 {
-    [System.Serializable]
-    public class InputDefinition
-    {
-        public InputActionName name;
-        public GameObject buttonPrefab;
-
-        public string joystickButtonName;
-        public string joystickAxixName;
-        public float joystickAxisThreshold;
-
-        public KeyCode keyboardKeyCode;
-
-        internal void DebugInWindow()
-        {
-            bool isActivated = IsActivated;
-            DebugWindow.Log ( "Input", name.ToString (), isActivated );
-        }
-
-        public bool IsActivated
-        {
-            get
-            {
-                if ( joystickButtonName.Length > 0 )
-                {
-                    return Input.GetButton ( joystickButtonName );
-                }
-
-                if ( joystickAxixName.Length > 0 )
-                {
-                    float value = Input.GetAxis ( joystickAxixName );
-                    if ( joystickAxisThreshold < 0 )
-                        return value < joystickAxisThreshold;
-                    else
-                        return value > joystickAxisThreshold;
-                }
-
-                if ( keyboardKeyCode != KeyCode.None )
-                {
-                    return Input.GetKey ( keyboardKeyCode );
-                }
-
-                Debug.LogError ( "Invalid input setup found for button " + name );
-                return false;
-            }
-        }
-    }
-
     internal GameObject GetComboButtonPrefab( InputActionName name )
     {
-        foreach ( InputDefinition def in CurrentDefinition )
+        foreach ( InputDefinition def in JoystickDefinition )
             if ( def.name == name )
                 return def.buttonPrefab;
 
@@ -75,41 +75,6 @@ public class InputManager : SceneSingleton<InputManager>
     }
 
     public InputDefinition[] JoystickDefinition;
-    public InputDefinition[] KeyboardDefinition;
 
-    internal InputDefinition[] CurrentDefinition
-	{
-		get
-		{
-			return JoystickDefinition;
-        }
-	}
-
-	internal List<InputActionName> NewInputsQueue = new List<InputActionName>();
-
-	List<InputDefinition> activeDefinitions = new List<InputDefinition>();
-
-    internal void Update ()
-	{
-        foreach ( InputDefinition def in CurrentDefinition )
-        {
-            def.DebugInWindow ();
-        }
-
-		foreach ( InputDefinition def in CurrentDefinition )
-		{
-			bool isActivated = def.IsActivated;
-			if ( isActivated )
-			{
-				if ( !activeDefinitions.Contains ( def ) )
-				{
-					activeDefinitions.Add ( def );
-					NewInputsQueue.Add ( def.name );
-				}
-			}
-			else
-				if ( activeDefinitions.Contains ( def ) )
-					activeDefinitions.Remove ( def );
-        }
-    }
+	
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
@@ -10,7 +11,19 @@ public class Player : MonoBehaviour
 		spineAnimation = GetComponent<SkeletonAnimation> ();
     }
 
-	void Update ()
+	internal InputDefinition[] CurrentControls
+	{
+		get
+		{
+			return InputManager.Instance.JoystickDefinition;
+		}
+	}
+
+	List<InputDefinition> activeDefinitions = new List<InputDefinition>();
+
+	internal List<InputActionName> inputsEntered = new List<InputActionName>();
+
+	void Update()
 	{
 		if ( Input.GetKeyDown ( KeyCode.F1 ) )
 			spineAnimation.AnimationName = "IDLE";
@@ -23,5 +36,79 @@ public class Player : MonoBehaviour
 
 		if ( Input.GetKeyDown ( KeyCode.F4 ) )
 			spineAnimation.AnimationName = "ANTE_ATTACK";
+
+		UpdateInputList ();
+
+		// 		foreach ( ComboPane cp in ActiveComboPanes )
+		// 		{
+		// 			List<InputActionName> combinaison = cp.combinaison;
+		// 			// combinaison.
+		// 		}
+
+		string inputsAsString = "";
+		foreach ( var v in inputsEntered )
+			inputsAsString += v.ToString () + " ";
+
+		if ( Input.GetKeyDown ( KeyCode.Backspace ) )
+			inputsEntered.Clear ();
+
+		DebugWindow.Log ( "Input", "List", inputsAsString );
+	}
+
+	void UpdateInputList()
+	{
+		List<InputActionName> newInputs = new List<InputActionName>();
+		UpdateInputs ( newInputs );
+		if ( newInputs.Count > 0 )
+			inputsEntered.AddRange ( newInputs );
+
+		bool oneCompleted = false;
+		bool onePartiallyCompleted = false;
+
+		foreach ( ComboPane cp in Game.Instance.ActiveComboPanes )
+			if ( !cp.isCompleted )
+			{
+				ComboPaneResolution resolution = cp.UpdateState ( inputsEntered );
+				switch ( resolution )
+				{
+					case ComboPaneResolution.Failed:
+						break;
+					case ComboPaneResolution.Completed:
+						oneCompleted = true;
+						break;
+					case ComboPaneResolution.NotCompleted:
+						onePartiallyCompleted = true;
+						break;
+				}
+			}
+
+		if ( oneCompleted )
+			inputsEntered.Clear ();
+		else if ( !onePartiallyCompleted )
+			inputsEntered.Clear ();
+	}
+
+	void UpdateInputs ( List<InputActionName> newInputs )
+	{
+		foreach ( InputDefinition def in CurrentControls )
+		{
+			def.DebugInWindow ();
+		}
+
+		foreach ( InputDefinition def in CurrentControls )
+		{
+			bool isActivated = def.IsActivated;
+			if ( isActivated )
+			{
+				if ( !activeDefinitions.Contains ( def ) )
+				{
+					activeDefinitions.Add ( def );
+					newInputs.Add ( def.name );
+				}
+			}
+			else
+				if ( activeDefinitions.Contains ( def ) )
+				activeDefinitions.Remove ( def );
+		}
 	}
 }
